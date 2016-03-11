46fc9451393b828b499933877901066f15db561d
Sergey Istomin <Sergey.Istomin@gmail.com>
2016-03-11 18:14:22 +0300

    Simple Data Parser

---
+++ accidents/items.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from peewee import Model, CharField, DateField, TimeField, ForeignKeyField, \
+    DecimalField, IntegerField, PostgresqlDatabase
+
+COORDINATES_LENGTH = 9
+COORDINATES_DECIMAL = 6
+
+db = PostgresqlDatabase('accidents',  # Required by Peewee.
+                        user='bars_web_bb',
+                        # Will be passed directly to psycopg2.
+                        password='bars_web_bb',
+                        host='localhost', )
+
+
+class Airfield(Model):
+    asn_id = CharField(20)
+    name = CharField(100)
+    ICAO = CharField(6)
+    IATA = CharField(6)
+    lat = DecimalField(max_digits=COORDINATES_LENGTH,
+                       decimal_places=COORDINATES_DECIMAL)
+    long = DecimalField(max_digits=COORDINATES_LENGTH,
+                        decimal_places=COORDINATES_DECIMAL)
+
+    class Meta:
+        database = db
+
+
+class Airline(Model):
+    asn_id = CharField(20)
+    name = CharField(100)
+    country = CharField(30)
+    ICAO = CharField(6)
+    IATA = CharField(6)
+    founded = DateField()
+    ended = DateField()
+
+    class Meta:
+        database = db
+
+
+class Weather(Model):
+    wind_speed_ff_ms = IntegerField()
+    wind_direction_ddd = DecimalField(max_digits=3, decimal_places=0)
+    visibility_VVVV = IntegerField()
+    VPP_visibility_VrVrVrVr = IntegerField()
+    weather = CharField(20)
+    cloudy = CharField(20)
+    temperature_cs = IntegerField()
+    dewpoint = IntegerField()
+    VPP = CharField(20)
+
+    class Meta:
+        database = db
+
+
+class EngineType(Model):
+    model_name = CharField(100)
+    country = CharField(30)
+    developed = DateField()
+    type = CharField(30)
+
+    class Meta:
+        database = db
+
+
+class AircraftType(Model):
+    asn_id = CharField(20)
+    ICAO = CharField(20)
+    first_flight_date = DateField()
+    production_total = IntegerField()
+    ICAO_mass_group = CharField(5)
+
+    class Meta:
+        database = db
+
+
+class Aircraft(Model):
+    registration = CharField(20)
+    model_serial_number = CharField(20)
+    aircraft_type = ForeignKeyField(AircraftType, related_name='aircrafts',
+                                    null=True)
+    asn_aircraft_type = CharField(20)
+    first_flight_date = DateField()
+    engines_number = IntegerField()
+    engine_type = ForeignKeyField(EngineType, related_name='aircrafts',
+                                  null=True)
+    asn_engine_type = CharField(20)
+
+    class Meta:
+        database = db
+
+
+class Accident(Model):
+    asn_id = CharField(20)
+    date = DateField()
+    time = TimeField()
+    aircraft = ForeignKeyField(Aircraft, related_name='accidents')
+    damage_type = CharField(max_length=100)
+    lat = DecimalField(max_digits=COORDINATES_LENGTH,
+                       decimal_places=COORDINATES_DECIMAL)
+    long = DecimalField(max_digits=COORDINATES_LENGTH,
+                        decimal_places=COORDINATES_DECIMAL)
+    flight_phase = CharField()
+    dep_airfield = ForeignKeyField(Airfield, related_name='dep_accidents')
+    asn_dep_airfield = CharField(20)
+    dep_weather = ForeignKeyField(Weather, related_name='department_accident')
+    dest_airfield = ForeignKeyField(Airfield, related_name='dest_accidents')
+    asn_dest_airfield = CharField(20)
+    dest_weather = ForeignKeyField(Weather, related_name='destination_accident')
+
+    class Meta:
+        database = db
+
+
+db.connect()
+
+Airfield.create_table(True)
+EngineType.create_table(True)
+Weather.create_table(True)
+AircraftType.create_table(True)
+Airline.create_table(True)
+Aircraft.create_table(True)
+Accident.create_table(True)
---
+++ accidents/spiders/get_accident.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from accidents.items import Accident, AircraftType, Airline, Airfield, \
+    EngineType, Aircraft, Weather
+
+
+class GetAccidentSpider(CrawlSpider):
+    name = 'get_accident'
+    allowed_domains = ['aviation-safety.net']
+    start_urls = ['https://aviation-safety.net/database/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'dblist\.php\?Year='), follow=True),
+        Rule(LinkExtractor(allow=r'database\/types\/.+\/index'), follow=True),
+        Rule(LinkExtractor(allow=r'database\/types\/.+\/specs'),
+             callback='parse_aircraft', follow=False),
+        Rule(LinkExtractor(allow=r'record\.php\?id='),
+             callback='parse_accident', follow=True),
+        Rule(LinkExtractor(allow=r'operator\/airline.php\?var='),
+             callback='parse_airline', follow=False),
+        Rule(LinkExtractor(allow=r'airport\/airport\.php\?id='),
+             callback='parse_airfield', follow=False),
+        Rule(LinkExtractor(allow=r'\/engine'), callback='parse_engine',
+             follow=False),
+    )
+
+    def parse_accident(self, response):
+        i = Accident()
+
+        i.asn_id = response.xpath('//input[@id="sid"]/@value').extract()
+        i.date = response.xpath('//input[@id="sid"]/@value').extract()
+        i.time = response.xpath('//input[@id="sid"]/@value').extract()
+
+        # extract aircraft
+        aircraft = Aircraft()
+        i.registration = response.xpath('//input[@id="sid"]/@value').extract()
+        i.model_serial_number = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.asn_aircraft_type = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.first_flight_date = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.engines_number = response.xpath('//input[@id="sid"]/@value').extract()
+        i.asn_engine_type = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+
+        i.aircraft = aircraft
+        i.damage_type = response.xpath('//input[@id="sid"]/@value').extract()
+        i.lat = response.xpath('//input[@id="sid"]/@value').extract()
+        i.long = response.xpath('//input[@id="sid"]/@value').extract()
+        i.flight_phase = response.xpath('//input[@id="sid"]/@value').extract()
+        i.asn_dep_airfield = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+
+        dep_weather = Weather()
+        i.wind_speed_ff_ms = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.wind_direction_ddd = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.vi.i.y_VVVV = response.xpath('//input[@id="sid"]/@value').extract()
+        i.VPP_vi.i.y_VrVrVrVr = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.weather = response.xpath('//input[@id="sid"]/@value').extract()
+        i.cloudy = response.xpath('//input[@id="sid"]/@value').extract()
+        i.temperature_cs = response.xpath('//input[@id="sid"]/@value').extract()
+        i.dewpoint = response.xpath('//input[@id="sid"]/@value').extract()
+        i.VPP = response.xpath('//input[@id="sid"]/@value').extract()
+        i.dep_weather = dep_weather
+
+        dest_weather = Weather()
+        i.wind_speed_ff_ms = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.wind_direction_ddd = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.vi.i.y_VVVV = response.xpath('//input[@id="sid"]/@value').extract()
+        i.VPP_vi.i.y_VrVrVrVr = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.weather = response.xpath('//input[@id="sid"]/@value').extract()
+        i.cloudy = response.xpath('//input[@id="sid"]/@value').extract()
+        i.temperature_cs = response.xpath('//input[@id="sid"]/@value').extract()
+        i.dewpoint = response.xpath('//input[@id="sid"]/@value').extract()
+        i.VPP = response.xpath('//input[@id="sid"]/@value').extract()
+
+        i.asn_dest_airfield = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.dest_weather = dest_weather
+
+        i.save()
+        return i
+
+    def parse_aircraft_type(self, response):
+        i = AircraftType()
+        i.asn_id = response.xpath('//input[@id="sid"]/@value').extract()
+        i.ICAO = response.xpath('//input[@id="sid"]/@value').extract()
+        i.first_flight_date = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.production_total = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+        i.ICAO_mass_group = response.xpath(
+            '//input[@id="sid"]/@value').extract()
+
+        return i
+
+    def parse_airline(self, response):
+        i = Airline()
+        i.isn_id = response.xpath('//input[@id="sid"]/@value').extract()
+        i.name = response.xpath('//input[@id="sid"]/@value').extract()
+        i.country = response.xpath('//input[@id="sid"]/@value').extract()
+        i.ICAO = response.xpath('//input[@id="sid"]/@value').extract()
+        i.IATA = response.xpath('//input[@id="sid"]/@value').extract()
+        i.founded = response.xpath('//input[@id="sid"]/@value').extract()
+        i.ended = response.xpath('//input[@id="sid"]/@value').extract()
+        return i
+
+    def parse_airfield(self, response):
+        i = Airfield()
+        i.isn_id = response.xpath('//input[@id="sid"]/@value').extract()
+        i.name = response.xpath('//input[@id="sid"]/@value').extract()
+        i.ICAO = response.xpath('//input[@id="sid"]/@value').extract()
+        i.IATA = response.xpath('//input[@id="sid"]/@value').extract()
+        i.lat = response.xpath('//input[@id="sid"]/@value').extract()
+        i.long = response.xpath('//input[@id="sid"]/@value').extract()
+        return i
+
+    def parse_engine(self, response):
+        i = EngineType()
+        i.model_name = response.xpath('//input[@id="sid"]/@value').extract()
+        i.country = response.xpath('//input[@id="sid"]/@value').extract()
+        i.developed = response.xpath('//input[@id="sid"]/@value').extract()
+        i.type = response.xpath('//input[@id="sid"]/@value').extract()
